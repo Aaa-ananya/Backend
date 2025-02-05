8a8be820f0d3e03a51671fb4e4399d9f6fa74942
Aaa-ananya <146376687+Aaa-ananya@users.noreply.github.com>
2025-02-05 09:44:37 +0530

    msg

--- ce/file_1.js
+++ ce/file_1.js
@@ -1,6 +1,15 @@
-const fs=require('node:fs');          //file system import
+const fs=require('node:fs');          //file system import\
+//****************************************Read file*********************************************** */
 // const data=fs.readFileSync('./data.txt');
 // console.log(data.toString());
 
-const data=fs.readFileSync('./data.txt','utf-8');
-console.log(data);
+// const data=fs.readFileSync('./data.txt',{encoding:'utf-8'});
+// console.log(data);
+
+
+//*******************************************Write File************************************** */
+fs.writeFileSync('./data1.txt','CE')
+fs.appendFileSync('./data1.txt','appended')
+
+//*******************************************Rename File************************************** */
+fs.renameSync('./data1.txt','./data2.txt')
