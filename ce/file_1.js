d5a64dbe107efcf6e05b6d31ae249bcd0d08d8e1
Aaa-ananya <146376687+Aaa-ananya@users.noreply.github.com>
2025-02-05 09:56:09 +0530

    m3

--- ce/file_1.js
+++ ce/file_1.js
@@ -3,13 +3,26 @@ const fs=require('node:fs');          //file system import\
 // const data=fs.readFileSync('./data.txt');
 // console.log(data.toString());
 
-// const data=fs.readFileSync('./data.txt',{encoding:'utf-8'});
-// console.log(data);
 
+const data=fs.readFileSync('./data.txt',{encoding:'utf-8'});
+console.log(data);
+if(data.match("h")){
+    console.log('h is present');
+    data.replace("h","ABCD");
+    fs.writeFileSync('./data.txt','changedata');
+}
 
 //*******************************************Write File************************************** */
-fs.writeFileSync('./data1.txt','CE')
-fs.appendFileSync('./data1.txt','appended')
+
+// fs.writeFileSync('./data1.txt','CE')
+// fs.appendFileSync('./data1.txt','appended')
 
 //*******************************************Rename File************************************** */
-fs.renameSync('./data1.txt','./data2.txt')
+
+// fs.renameSync('./data1.txt','./data2.txt')
+
+//*******************************************Delete File************************************** */
+
+// fs.unlinkSync('./data2.txt')
+
+//*******************************************Delete File************************************** */
