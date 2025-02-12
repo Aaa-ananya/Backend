d31f788e26cb1d002e717614215a214157e3420f
Aaa-ananya <146376687+Aaa-ananya@users.noreply.github.com>
2025-02-12 09:18:27 +0530

    msg

--- ce/fileasync.js
+++ ce/fileasync.js
@@ -1,1 +1,16 @@
-const fs=require('./fs');
+const { log } = require('console');
+const fs=require('fs/promises');
+const read=async()=>{
+   const data=await  fs.readFile("./data.txt",'utf-8');
+   console.log(data);
+}
+const write=async()=>{
+    const newdata='This is new data on 12 feb';
+    const data=await  fs.writeFile("./data1.txt",newdata,'utf-8');
+ }
+read();
+write();
+console.log(1);
+console.log(2);
+
+
