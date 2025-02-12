f02f25390a467f171f4af28d7ee9836c859a3e77
Aaa-ananya <146376687+Aaa-ananya@users.noreply.github.com>
2025-02-12 09:00:22 +0530

    msg

---
+++ ce/file2.mjs
@@ -0,0 +1,10 @@
+const summ=( a, b)=>{
+return a+b;
+}
+
+const multiply=( a, b)=>{
+    return a*b;
+    }
+    
+export {summ};
+export {multiply};
---
+++ ce/file3.mjs
@@ -0,0 +1,5 @@
+import { summ } from "./file2.mjs";
+import { multiply } from "./file2.mjs";
+
+console.log(summ(2,3));
+console.log(multiply(3,4));
